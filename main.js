8dee06d56f8ad1c8590b43f371969f1b3a8359cf
haru-pyn <thma_119210@yahoo.co,jp>
2022-09-07 23:29:38 +0900

    入力して追加機能

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
 new Vue({
-    el: '#mo-co'
+    el: '#mo-co',
+    data:{
+        newTitle:'',
+        newCategory:'',
+        newMemo:'',
+        uniqueKey:0,
+        Movies:[]
+    },
+    mounted(){
+        this.getUniqueKey()
+    },
+    methods:{
+        getUniqueKey: function(){
+            if(this.Movies.length === 0){
+                return
+            }
+
+            let maxUniqueKey = 0
+            this.Movies.forEach(function(movie){
+                if(maxUniqueKey < movie.id){
+                    maxUniqueKey = movie.id
+                }
+            });
+            this.uniqueKey = maxUniqueKey
+        },
+        addMovie: function(){
+            if (this.newTitle === '')return;
+            this.Movies.push(
+                {
+                    title: this.newTitle,
+                    category: this.newCategory,
+                    memo: this.newMemo,
+                    id: ++this.uniqueKey
+                }
+            );
+            this.initTitle;
+            this.initMemo;
+        },
+        initTitle: function(){
+            this.newTitle = '';
+        },
+        initMemo: function(){
+            this.newMemo = '';
+        },
+        
+    }
 })
